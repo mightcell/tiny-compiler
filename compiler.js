e61954453153fb8e34a6992f021641ebe6b769fa
mightcell <89257166+mightcell@users.noreply.github.com>
2023-05-26 11:04:01 +0800

    feat：create a file which will return a matching Javascript statement

---
+++ compiler.js
@@ -0,0 +1,9 @@
+module.exports = function compiler(input) {
+    // 1. Lexical Analysis
+    // 2. Syntactic Analysis
+    // 3. Transformation
+    // 4. Code Generation
+
+    // return jsCode
+
+}
