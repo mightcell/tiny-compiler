d1046cdc6f66f86d2140963f8f67cac9202dab85
mightcell <89257166+mightcell@users.noreply.github.com>
2023-05-26 11:07:49 +0800

    feat: create lexical analysis file

--- compiler.js
+++ compiler.js
@@ -1,9 +1,14 @@
+const tokenizer = require('./tokenizer');
 module.exports = function compiler(input) {
     // 1. Lexical Analysis
+    //      Breaks the input code (string) into the basic block
+    //      of the language (array of objects)
+    const tokens = tokenizer(input);
     // 2. Syntactic Analysis
     // 3. Transformation
     // 4. Code Generation
-
+    return tokens;
     // return jsCode
+
 
 }
