d17b409ad782c28b82d0ba49e5f5d3c420fb146e
mightcell <89257166+mightcell@users.noreply.github.com>
2023-05-26 11:47:06 +0800

    feat：finish syntactic analysis which will transform the tokens into an AST

--- compiler.js
+++ compiler.js
@@ -1,13 +1,18 @@
 const tokenizer = require('./tokenizer');
+const parser = require('./parser')
 module.exports = function compiler(input) {
     // 1. Lexical Analysis
     //      Breaks the input code (string) into the basic block
     //      of the language (array of objects)
     const tokens = tokenizer(input);
     // 2. Syntactic Analysis
+    //      Transformation the tokens (array of objects) into an
+    //      AST (tree of objects) which represents our program
+    const lispAST = parser(tokens);
+    return lispAST;
     // 3. Transformation
     // 4. Code Generation
-    return tokens;
+
     // return jsCode
 
 
